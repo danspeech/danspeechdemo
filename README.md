b6c62994e075e65252ec53b12a0633ec1e492b48
Rasmus Arpe Fogh Jensen <arpelarpe@Rasmuss-MacBook-Pro-6.local>
2019-08-30 14:23:23 +0200

    Demo first draft

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # danspeechdemo
-Danspeech webapp demo 
+DanSpeech demo.
+
+This is a demo that demonstrates some of the functionality of the [danspeech](https://github.com/danspeech/danspeech) python package. 
+
+
+## Installation
+Note that the demo includes some functionality of Danspeech that requires [PyAudio](https://pypi.org/project/PyAudio/) and [ctcdecoce](https://github.com/parlance/ctcdecode). 
+
+For the best possible experience, please download and install these libraries as well! 
+
+Else, the following will enable danspeech.
+
+```bash
+$ pip install danspeech
+$ pip install django
+$ git clone https://github.com/danspeech/danspeechdemo.git
+$ cd danspeechdemo
+$ pip install .
+```
+
+
+## Usage
+Now, execute a script or run from terminal as below:
+
+```bash
+
+$ python
+>>> import danspeechdemo
+>>> danspeechdemo.run_server()
+```
+
+The demo will then be live at [http://127.0.0.1:8000](http://127.0.0.1:8000).
+
+## This demo is currently under development!
+
+Features not working correctly:
+
+* Streaming
+* Folketinget example
+
+These features will be added as soon as possible.
--- danspeechdemo/demo/views.py
+++ danspeechdemo/demo/views.py
@@ -8,10 +8,10 @@ import json
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
-from .models_mapping import get_danspeech_model, get_danspeech_lm
-
 from danspeech import Recognizer
 from danspeech.audio.resources import load_audio_wavPCM, load_audio, Microphone
+# We need a default model. Will use the TestModel as example.
+from danspeech.pretrained_models import TestModel
 
 if "DANSPEECH_GPU" in os.environ:
     with_gpu = os.environ["DANSPEECH_GPU"] == "1"
@@ -25,74 +25,103 @@ counter = 0
 
 def index(request):
     template = loader.get_template('index.html')
-
-    # Default update model
-    from danspeech.pretrained_models import Units400
-    model = Units400()
+    model = TestModel()
     recognizer.update_model(model)
 
     return HttpResponse(template.render({}, request))
 
 
-def multi(request):
-    template = loader.get_template('multi.html')
-    return HttpResponse(template.render({}, request))
+def update_config(request):
+    model_identifier = int(request.POST["model_choice"])
+    lm_identifier = int(request.POST["language_model"])
+    alpha = float(request.POST["alpha"])
+    beta = float(request.POST["beta"])
+    beam_width = int(request.POST["beam"])
 
-
-def stream(request):
-    template = loader.get_template('stream.html')
-    from danspeech.pretrained_models import StreamingRNN
-    model = StreamingRNN()
+    # Update model
+    model = get_danspeech_model(model_identifier)
     recognizer.update_model(model)
-    microphones = [str(m) for m in Microphone.list_microphone_names()]
-    mic_list_with_numbers = list(zip(range(len(microphones)), microphones))
-    mic_list = json.dumps(mic_list_with_numbers)
-    print(mic_list)
-    return HttpResponse(template.render({"mic_list": mic_list}, request))
+
+    # Update lm
+    lm = get_danspeech_lm(lm_identifier)
+    recognizer.update_decoder(lm, alpha=alpha, beta=beta, beam_width=beam_width)
+
+    return HttpResponse(status=204)
 
 
-def update_microphone(request):
-    mic_id = int(request.POST["mic_id"])
-    recognizer.microphone = Microphone(sampling_rate=16000, device_index=int(mic_id))
-    print("Updated mic")
-    return JsonResponse({
-        'success': True,
-    })
+def get_danspeech_model(id):
+    """
+    Gets a DanSpeech model based on ID from front-end.
+
+    We need to do it this way to avoid downloading unnecessary models.
+    """
+
+    if id == 0:
+        return TestModel()
+    elif id == 1:
+        from danspeech.pretrained_models import DanSpeechPrimary
+        return DanSpeechPrimary()
+    elif id == 2:
+        from danspeech.pretrained_models import Baseline
+        return Baseline()
+    elif id == 3:
+        from danspeech.pretrained_models import TransferLearned
+        return TransferLearned()
+    elif id == 4:
+        from danspeech.pretrained_models import Folketinget
+        return Folketinget()
+    elif id == 5:
+        from danspeech.pretrained_models import EnglishLibrispeech
+        return EnglishLibrispeech()
+    else:
+        print("Model identifier not valid... Defaulting to TestModel.")
+        # If nothing is given, just return TestModel. Something has then gone wrong.
+        return TestModel()
 
 
-def streaming_generator():
-    generator = recognizer.microphone_streaming(recognizer.microphone)
-    while True:
-        try:
-            is_last, trans = next(generator)
-            # If the transcription is empty, it means that the energy level required for data
-            # was passed, but nothing was predicted.
-            if trans:
-                if is_last:
-                    yield trans
-                else:
-                    yield trans
+def get_danspeech_lm(id):
+    """
+    Gets a DanSpeech LM based on ID from front-end.
 
-        except StopIteration:
-            break
+    We need to do it this way to avoid downloading unnecessary models.
+    """
 
-
-def start_streaming(request):
-    with recognizer.microphone as source:
-        print("Adjusting for background noise")
-        recognizer.adjust_for_ambient_noise(source)
-    stream_gen = streaming_generator()
-    response = StreamingHttpResponse(stream_gen, status=200, content_type='text/event-stream')
-    response['Cache-Control'] = 'no-cache'
-
-    return response
-
-
-def stop_streaming(request):
-    recognizer.stop_microphone_streaming()
-    return JsonResponse({
-        'success': True,
-    })
+    if id == 0:
+        return "greedy"
+    elif id == 1:
+        from danspeech.language_models import DSL3gram
+        return DSL3gram()
+    elif id == 2:
+        from danspeech.language_models import DSL5gram
+        return DSL5gram()
+    elif id == 3:
+        from danspeech.language_models import DSLWiki3gram
+        return DSLWiki3gram()
+    elif id == 4:
+        from danspeech.language_models import DSLWiki5gram
+        return DSLWiki5gram()
+    elif id == 5:
+        from danspeech.language_models import DSLWikiLeipzig3gram
+        return DSLWikiLeipzig3gram()
+    elif id == 6:
+        from danspeech.language_models import DSLWikiLeipzig5gram
+        return DSLWikiLeipzig5gram()
+    elif id == 7:
+        from danspeech.language_models import Wiki3gram
+        return Wiki3gram()
+    elif id == 8:
+        from danspeech.language_models import Wiki5gram
+        return Wiki5gram()
+    elif id == 9:
+        from danspeech.language_models import Folketinget3gram
+        return Folketinget3gram()
+    elif id == 10:
+        from danspeech.language_models import DSL3gramWithNames
+        return DSL3gramWithNames()
+    else:
+        print("Model identifier not valid... Defaulting to TestModel.")
+        # If nothing is given, just return TestModel. Something has then gone wrong.
+        return TestModel()
 
 
 def preprocess_webm(request):
@@ -112,24 +141,6 @@ def preprocess_webm(request):
     })
 
 
-def update_config(request):
-    model_identifier = request.POST["model_choice"]
-    lm_identifier = request.POST["language_model"]
-    alpha = float(request.POST["alpha"])
-    beta = float(request.POST["beta"])
-    beam_width = int(request.POST["beam"])
-
-    # Update model
-    model = get_danspeech_model(model_identifier)
-    recognizer.update_model(model)
-
-    # Update lm
-    lm = get_danspeech_lm(lm_identifier)
-    recognizer.update_decoder(lm, alpha=alpha, beta=beta, beam_width=beam_width)
-
-    return HttpResponse(status=204)
-
-
 def transcribe(request):
     audio = load_audio_wavPCM(path=os.path.join(settings.MEDIA_ROOT, "tmp.wav"))
     transcription = recognizer.recognize(audio, show_all=False)
@@ -137,6 +148,11 @@ def transcribe(request):
         'success': True,
         'trans': transcription
     })
+
+
+def multi(request):
+    template = loader.get_template('multi.html')
+    return HttpResponse(template.render({}, request))
 
 
 def send_audio_filepath(request):
@@ -169,4 +185,83 @@ def send_audio_filepath(request):
         'success': True,
         'transcriptions': transcriptions,
         'fnames': fnames
+    })
+
+
+def update_microphone(request):
+    mic_id = int(request.POST["mic_id"])
+    recognizer.microphone = Microphone(sampling_rate=16000, device_index=int(mic_id))
+    print("Updated mic")
+    return JsonResponse({
+        'success': True,
+    })
+
+
+def stream(request):
+    template = loader.get_template('stream.html')
+    from danspeech.pretrained_models import TestModel
+    model = TestModel()
+    recognizer.update_model(model)
+    microphones = [str(m) for m in Microphone.list_microphone_names()]
+    mic_list_with_numbers = list(zip(range(len(microphones)), microphones))
+    mic_list = json.dumps(mic_list_with_numbers)
+    print(mic_list)
+    return HttpResponse(template.render({"mic_list": mic_list}, request))
+
+
+def start_streaming(request):
+    with recognizer.microphone as source:
+        print("Adjusting for background noise")
+        recognizer.adjust_for_speech(source, duration=5)
+
+    recognizer.enable_streaming()
+    generator = recognizer.streaming(recognizer.microphone)
+    response = StreamingHttpResponse(generator, status=200, content_type='text/event-stream')
+    response['Cache-Control'] = 'no-cache'
+    return response
+
+
+def stop_streaming(request):
+    recognizer.disable_streaming()
+    return JsonResponse({
+        'success': True,
+    })
+
+
+def streaming_generator():
+    generator = recognizer.streaming(recognizer.microphone)
+    while True:
+        try:
+            trans = next(generator)
+        except StopIteration:
+            break
+
+def real_time_stream(request):
+    template = loader.get_template('real_time_stream.html')
+    from danspeech.pretrained_models import CPUStreamingRNN
+    model = CPUStreamingRNN()
+    recognizer.update_model(model)
+    microphones = [str(m) for m in Microphone.list_microphone_names()]
+    mic_list_with_numbers = list(zip(range(len(microphones)), microphones))
+    mic_list = json.dumps(mic_list_with_numbers)
+    print(mic_list)
+    return HttpResponse(template.render({"mic_list": mic_list}, request))
+
+
+def start_real_time_streaming(request):
+    with recognizer.microphone as source:
+        print("Adjusting for background noise")
+        recognizer.adjust_for_speech(source, duration=5)
+
+    recognizer.enable_real_time_streaming()
+    generator = recognizer.streaming(recognizer.microphone)
+    response = StreamingHttpResponse(generator, status=200, content_type='text/event-stream')
+    response['Cache-Control'] = 'no-cache'
+    return response
+
+
+def stop_real_time__streaming(request):
+    recognizer.disable_streaming()
+    return JsonResponse({
+        'success': True,
     })
